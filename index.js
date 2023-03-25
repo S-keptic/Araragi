a44d6e2a1ad2e5ca883d4cc9b049aa004db99aba
sekire <69573771+leviackermannn@users.noreply.github.com>
2023-03-25 23:28:36 +0530

    3

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-require('dotenv').config()
-console.log("hi")
+const { Client, GatewayIntentBits, InteractionType } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { EmbedBuilder } = require('discord.js');
+require('dotenv').config();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
