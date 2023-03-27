ad5e06e038954a8fc80dcab81b4f71aa98e978e6
sekire <69573771+leviackermannn@users.noreply.github.com>
2023-03-28 00:54:15 +0530

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const { EmbedBuilder } = require('discord.js');
 require('dotenv').config();
 const mongoose = require('mongoose');
-
+const openai = require('openai');
 const mon = ()=>{
   mongoose.connect(process.env.MONGODB_URL,{keepAlive: true});
   console.log("connected to db")
@@ -20,7 +20,14 @@ client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+    
+
+
+
+interaction.reply("you are a nigger")
+
+
+
   }
 });
 
